dbef922f1c5cbda573c27769f0237394f8c17dbb
Joe Roberts <joe@zefer.co.uk>
2012-02-01 09:17:18 +0000

    simple list output of <img> tags

--- js/app.js
+++ js/app.js
@@ -1,7 +1,17 @@
 var Fmber = Em.Application.create();
 
-Fmber.MyView = Em.View.extend({
-  mouseDown: function() {
-    window.alert("hello world!");
-  }
+Fmber.Image = Em.Object.extend({
+  src: null
 });
+
+Fmber.imagesController = Em.ArrayProxy.create({
+  content: [],
+
+  addImage: function(src) {
+    var image = Fmber.Image.create({ src: src });
+    this.pushObject(image);
+  },
+});
+
+Fmber.imagesController.addImage("http://img.ffffound.com/static-data/assets/6/8d38d0bda049eddae66b0ab7d23e7198f68a4f2b_m.jpg");
+Fmber.imagesController.addImage("http://img.ffffound.com/static-data/assets/6/8d38d0bda049eddae66b0ab7d23e7198f68a4f2b_m.jpg");
