69dd34387a2f2ecf1d6d1766f15972752c66e627
Joe Roberts <joe@zefer.co.uk>
2012-02-01 11:40:33 +0000

    put loading of images into the controller

--- js/app.js
+++ js/app.js
@@ -23,15 +23,19 @@ Fmber.imagesController = Em.ArrayProxy.create({
 			  });
 			});
 		}, 500);
+  },
+
+  updateImages: function() {
+  	// load images from ffffound.com via YQL
+		yql = "http://query.yahooapis.com/v1/public/yql?q=select%20title%2C%20link%2C%20description%2C%20author%2C%20pubDate%2C%20media%3Acontent%2C%20media%3Athumbnail%2C%20ffffound%3Asavedby%20from%20rss%20where%20url%3D%22http%3A%2F%2Ffeeds.feedburner.com%2Fffffound%2Feveryone%22&format=json&callback=?";
+		$.getJSON(yql, function(data) {
+			data.query.results.item.forEach(function(item) {
+			  Fmber.imagesController.addImage(item.content.url);
+			});
+
+			Fmber.imagesController.display();
+		});
   }
 });
 
-// load images from ffffound.com via YQL
-yql = "http://query.yahooapis.com/v1/public/yql?q=select%20title%2C%20link%2C%20description%2C%20author%2C%20pubDate%2C%20media%3Acontent%2C%20media%3Athumbnail%2C%20ffffound%3Asavedby%20from%20rss%20where%20url%3D%22http%3A%2F%2Ffeeds.feedburner.com%2Fffffound%2Feveryone%22&format=json&callback=?";
-$.getJSON(yql, function(data) {
-	data.query.results.item.forEach(function(item) {
-	  Fmber.imagesController.addImage(item.content.url);
-	});
-
-	Fmber.imagesController.display();
-});
+Fmber.imagesController.updateImages();
